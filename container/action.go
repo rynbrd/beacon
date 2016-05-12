e44e6e3c3b7b969aeafd892a688bbedf060aeafc
Ryan Bourgeois <bluedragonx@gmail.com>
2016-05-12 13:03:03 -0700

    container: Remove unused package.

--- container/action.go
+++
@@ -1,9 +0,0 @@
-package container
-
-type Action int
-
-const (
-	// Container event actions.
-	Add Action = iota
-	Remove
-)
--- container/event.go
+++
@@ -1,8 +0,0 @@
-package container
-
-// Event is used by the Listener to notify Beacon of
-// container actions.
-type Event struct {
-	Action    Action
-	Container *Container
-}
